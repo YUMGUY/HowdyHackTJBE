2f15c2f1f741b2d7bb2a9b58be7a5349a299b4e3
ehh-git <elliothan@tamu.edu>
2022-10-02 01:45:33 -0500

    randomizer/database added

--- scriptmaker.py
+++ scriptmaker.py
@@ -1,7 +1,7 @@
-with open("websiteCode/formated200.txt") as f:
+with open("formattedData/formated200.txt") as f:
     list100 = f.readlines()
 import sys
-sys.stdout = open('sofia.json', 'w')
+sys.stdout = open('sofia.js', 'w')
 for i in range(len(list100)):
     list100[i] = list100[i] + ", "
     print(list100[i], end = '')
